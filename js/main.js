07ea4f19c7fa9ea8607b1cd04abb1879bde89c21
KefirchickTod <37833366+KefirchickTod@users.noreply.github.com>
2021-02-25 17:59:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,15 +15,16 @@ let QueryGet = function () {
         );
 };
 const userDataJson = 'https://api.npoint.io/9736691522f21f75e1c5';
-
+//Селектор (HTML - тег) в якому зберігається загальна сума
 let totalPriceSelector = $(".totalPrice"); //0
-
+//Оголошуєм (створює функція ) з аргументами селектор і дія (дія - true(+) || false(-1))  
 function calcPlusMinus(selector, action){
-    let $this = $(selector);
-       let parentSelector = $this.parent();
+    //
+    let $this = $(selector); // Обгортаєм селекторв  в обєкт jquery
+       let parentSelector = $this.parent(); // отрмаєм батьківський елемент для того щоб найти необхідний елемент
 
     /** Отримати поточну ціну товару */
-    let currentPrice = parentSelector.parent().find('.unitPrice').text(); //800
+    let currentPrice = parentSelector.parent().find('.unitPrice').text(); //800 шукаємо ціну товару
     
     /** Отримати кількіст вибраного товару */
     let currentNumber = parentSelector.find('.number').text(); //0;
@@ -31,29 +32,29 @@ function calcPlusMinus(selector, action){
     /** Отримати поточну загальну ціну усіх товарів в кошику */
     let currentTotalPrice = totalPriceSelector.text(); //0
 
-    if(action === false){
+    if(action === false){ //Логіка якшо дія віднімання 
         currentNumber = parseInt(currentNumber) - 1;
         
-        if(currentNumber < 0){
+        if(currentNumber < 0){ //якщо кількість вибраних товарів менше 1 виходимо з функції
             return;
         }
             currentTotalPrice = parseInt(currentTotalPrice) - parseInt(currentPrice);
 
     }
-    if(action === true){
+    if(action === true){//Якшо дія додавання
     currentNumber = parseInt(currentNumber) + plusStep;
         currentTotalPrice = parseInt(currentTotalPrice) + parseInt(currentPrice);
     }
 
  
 
-    parentSelector.find('.number').text(currentNumber);
-    totalPriceSelector.text(currentTotalPrice);
+    parentSelector.find('.number').text(currentNumber); //присвоюєм кількість +1
+    totalPriceSelector.text(currentTotalPrice);//виводимо загальну суму
 
 }
 
 $('.plus').click(function(){
-    calcPlusMinus($(this), true);
+    calcPlusMinus($(this), true); //$(this) - селектор, тру дія
 });
 
 $('.moins').click(function(){
